7f769902c05f3ed02a0169e9eb518ac3b5b018c1
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2025-12-15 01:10:43 +0000

    Added basic benchmarking script

---
+++ benchmark.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Basic benchmark script comparing searchfs with find
+# Tests on root filesystem only
+
+# searchfs
+time ./searchfs -v / Foundation &> /dev/null
+
+# find - xdev flag ensures other filesystems are not traversed
+time find / -xdev -name '*Foundation*' &> /dev/null
